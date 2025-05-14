293990691436490d4cb547f26c2f531dec99a9ef
Rahul6701 <rahul.elgoss@gmail.com>
2025-05-14 12:44:14 +0530

    Delete StoryBook directory

--- StoryBook/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StoryBook.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
